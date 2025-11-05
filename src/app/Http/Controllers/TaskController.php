eb15d380eba044a55d5e7e189b0a21c633d78e86
malutkazombi <gothamexplode@gmail.com>
2025-11-05 09:26:05 +0900

    6

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Folder;
 use App\Models\Task;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -20,7 +21,7 @@ class TaskController extends Controller
 
         $folder = Folder::find($id);
 
-         $tasks = $folder->tasks()->get();
+        $tasks = $folder->tasks()->get();
 
         return view(
             'tasks/index',
@@ -30,5 +31,22 @@ class TaskController extends Controller
                 'tasks' => $tasks
             ]
         );
+    }
+    public function showCreateForm(int $id)
+    {
+        return view('tasks/create', ['folder_id' => $id]);
+    }
+    public function create(int $id, CreateTask $request)
+    {
+        $folder = Folder::find($id);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
+        $folder->tasks()->save($task);
+
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id,
+        ]);
     }
 }
